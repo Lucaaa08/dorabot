61296bba02f63adc705b973944f05a89d3c95cec
Dora-emond <85222479+Dora-emond@users.noreply.github.com>
2021-12-28 06:18:52 +0700

    Update waifu.js

--- plugins/waifu.js
+++ plugins/waifu.js
@@ -1,20 +1,13 @@
 let fetch = require('node-fetch')
-let handler = async(m, { conn, args, usedPredix, command }) => {
-    let res = await fetch('https://api.waifu.pics/sfw/waifu')
-    if (!res.ok) throw eror
-    let json = await res.json()
-    if (!json.url) throw 'Error!'
-    if (args[0] == '--help') await conn.send2Button(m.chat, `
-Feature support
-query: ${usedPredix + command}
 
-Fitur ini adalah fitur untuk mendapatkan foto waifu secara random.
-Penggunaan: *${usedPredix + command}*
-
-Fitur ini menggunakan limit, upgrade menjadi user premium untuk menggunakan tanpa batasan limit.`.trim(), watermark, '🔍Search WAIFU', '.waifu', '⭐Premium', '.premium', m)
-    if (!args[0]) await conn.sendButtonImg(m.chat, await(await fetch(json.url)).buffer(), 'WAIFU image', watermark, '🔍Search WAIFU', '.waifu', m)
+let handler = async (m, { conn, usedPrefix, command }) => {
+  let res = await fetch(API('waifupics', '/sfw/waifu'))
+  if (!res.ok) throw eror
+  let json = await res.json()
+  conn.sendButtonImg(m.chat, json.url, 'Waifu', '© stikerin', 'Waifu', usedPrefix + command, m)
 }
 handler.help = ['waifu']
 handler.tags = ['internet']
-handler.command = /^waifu$/i
-module.export = handler
+handler.command = /^(waifu)$/i
+
+module.exports = handler
