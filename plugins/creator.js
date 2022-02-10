e9e692b07fa8cc4358e4915b5d4e63c52eb0daf1
Lucaaa08 <97221320+Lucaaa08@users.noreply.github.com>
2022-02-10 19:02:57 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 function handler(m) {
   m.reply('Chat jika penting, harap tidak spam.')
-  this.sendContact(m.chat, '6289601145546@s.whatsapp.net', 'Dora', m)
+  this.sendContact(m.chat, '6281808882889@s.whatsapp.net', 'Haru', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
